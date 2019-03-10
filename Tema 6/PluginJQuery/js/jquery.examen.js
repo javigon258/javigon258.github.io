2a37354909c55dedae7bea7da020e43318987905
javier <javigon258@gmail.com>
2019-03-10 13:43:57 +0100

    Cambios en tester y patrones

--- Tema 6/PluginJQuery/js/jquery.examen.js
+++ Tema 6/PluginJQuery/js/jquery.examen.js
@@ -1,18 +1,18 @@
 (function ($) {
 
-    $.fn.examen = function (resalto2) {
+    let patrones = {
+        nombre: [/^([a-zA-Z]{3,}\s?)$/, "El valor minimo debe tener 3 caracteres."],
+        email: [/^[a-zA-Zñ]{1,10}([.][a-zA-Zñ]{1,10}){0,3}[@][a-z]{1,6}([\.][a-z]{1,4}){1,4}$/, "Un ejemplo de correo: ejemplo@gmail.com"],
+        mensaje: [/^^.{20,}$/, "El contenido del mensaje debe tener 20 caracteres como minimo."],
 
-        let patrones = {
-            nombre: [/^([a-zA-Z]{3,}\s?)$/, "El valor minimo debe tener 3 caracteres."],
-            email: [/^[a-zA-Zñ]{1,10}([.][a-zA-Zñ]{1,10}){0,3}[@][a-z]{1,6}([\.][a-z]{1,4}){1,4}$/, "Un ejemplo de correo: ejemplo@gmail.com"],
-            mensaje: [/^^.{20,}$/, "El contenido del mensaje debe tener 20 caracteres como minimo."],
-    
-        }  
-        let tester = {
-            valida(texto, expresion) {
-                return patrones[expresion][0].test(texto);
-            }            
-        }
+    }  
+    let tester = {
+        valida(texto, expresion) {
+            return patrones[expresion][0].test(texto);
+        }            
+    }
+
+    $.fn.examen = function (resalto2) {
 
         let resalto = {
             color: "#ff0000",
