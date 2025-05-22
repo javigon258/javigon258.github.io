ca5f24c54c18c8ae941b6cf3ecb4024b8416428a
Javier González <javigon258@gmail.com>
2025-05-22 09:59:04 +0000

    cambios listado

--- src/components/layout/Clientes.jsx
+++ src/components/layout/Clientes.jsx
@@ -8,13 +8,17 @@ const Clientes = () => {
 					<h3 className="titulo">Imagenes de prueba</h3>
 					<p className="subtitulo"></p>
 				</div>
-				<div className="logos">
-					<img src="./assets/logos/Recharge.svg" className="logo" alt="Logo de Recharge" />
-					<img src="./assets/logos/Volume.svg" className="logo" alt="Logo de Volume" />
-					<img src="./assets/logos/Umbrella.svg" className="logo" alt="Logo de Umbrella" />
-					<img src="./assets/logos/Waveless.svg" className="logo" alt="Logo de Waveless" />
-					<img src="./assets/logos/Vision.svg" className="logo" alt="Logo de Vision" />
-				</div>
+				{
+					/*
+					<div className="logos">
+						<img src="./assets/logos/Recharge.svg" className="logo" alt="Logo de Recharge" />
+						<img src="./assets/logos/Volume.svg" className="logo" alt="Logo de Volume" />
+						<img src="./assets/logos/Umbrella.svg" className="logo" alt="Logo de Umbrella" />
+						<img src="./assets/logos/Waveless.svg" className="logo" alt="Logo de Waveless" />
+						<img src="./assets/logos/Vision.svg" className="logo" alt="Logo de Vision" />
+					</div>
+					*/
+				}
 			</section>
 		</>
 	);
